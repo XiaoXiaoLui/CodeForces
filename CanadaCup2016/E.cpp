7016efb9e21f5a3e7b586e675eb2c8bd279ad87a
TinyTiny <980821718@qq.com>
2016-10-27 03:09:55 +0800

    CanadaCup

--- CanadaCup2016/E.cpp
+++ CanadaCup2016/E.cpp
@@ -14,11 +14,81 @@ using namespace std;
 
 typedef long long ll;
 
-
+const int N = 2e5 + 100;
+pii a[N];
 
 int main()
 {
 	// USE_IOSTREAM;
+	int n, wantSum, i, j, ans, cur, maxNum;
+	scanf("%d%d", &wantSum, &n);
+	for (i = 1, maxNum = 0; i <= n; i++)
+	{
+		scanf("%d", &cur);
+		a[cur].first++;
+		maxNum = max(maxNum, cur);
+	}
+	
+	cur = 0;
+	for (i = 1; i <= wantSum; i++)
+	{
+		a[i].second = cur;
+		if (a[i].first)
+		{
+			cur = i;
+		}
+	}
+	
+	bool ok = false;
+	for (ans = 1; ans < wantSum; ans++)
+	{
+		cur = wantSum;
+		i = a[cur].first ? cur : a[cur].second;
+		bool used = false;
+		while (cur > 0)
+		{
+			if (!used && ans <= cur && ans >= i)
+			{
+				used = true;
+				cur -= ans;
+				if (cur < i)
+				{
+					i = a[cur].first ? cur : a[cur].second;
+				}
+			}
+			else if (i == 0)
+			{
+				ok = true;
+				break;
+			}
+			else 
+			{
+				cur -= min(a[i].first, cur / i) * i;
+				if (cur >= i)
+				{
+					i = a[i].second;
+				}
+				else
+				{
+					i = a[cur].first ? cur : a[cur].second;
+				}	
+			}	
+		}
+		
+		if (ok)
+		{
+			break;
+		}
+	}
+	
+	if (ok)
+	{
+		printf("%d\n", ans);
+	}
+	else
+	{
+		printf("Greed is good\n");
+	}
 	
 	return 0;
 }
--- CanadaCup2016/F.cpp
+++ CanadaCup2016/F.cpp
@@ -14,11 +14,56 @@ using namespace std;
 
 typedef long long ll;
 
+const int N = 1e5 + 100;
+
+vector<pii> srt;
+
+bool cmp(const pii &a, const pii &b)
+{
+	return (a.first + a.second) > (b.first + b.second);
+}
 
 
 int main()
 {
 	// USE_IOSTREAM;
+	int n, i, a1, b1, a2, b2;
+	scanf("%d", &n);
+	
+	for (i = 1; i <= n; i++)
+	{
+		scanf("%d%d%d%d", &a1, &b1, &a2, &b2);
+		
+		if (a1 + b1 >= a2 + b2)
+		{
+			srt.push_back(mp(a1, b1));
+			srt.push_back(mp(a2, b2));
+		}
+		else if (a1 > b2)
+		{
+			srt.push_back(mp(a1 - b2, b2 - a1));
+		}
+		else if (b1 > a2)
+		{
+			srt.push_back(mp(a2 - b1, b1 - a2));
+		}
+	}
+	
+	ll ans = 0;
+	sort(srt.begin(), srt.end(), cmp);
+	for (i = 0; i < srt.size(); i++)
+	{
+		if (i & 1)
+		{
+			ans -= srt[i].second;
+		}
+		else
+		{
+			ans += srt[i].first;
+		}
+	}
+	
+	printf("%lld\n", ans);
 	
 	return 0;
 }
